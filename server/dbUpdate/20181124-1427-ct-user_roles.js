4426ba5f097d62e618ef35ee6a61288d5d208e6a
StepanPG <stepan-mandarin@yandex.ru>
2018-11-24 15:05:57 +0300

    fix: rearranged table columns

--- server/dbUpdate/20181124-1427-ct-user_roles.js
+++ server/dbUpdate/20181124-1427-ct-user_roles.js
@@ -2,16 +2,16 @@ async function task({ pgAsync }) {
     await pgAsync.transaction(async (pgAsync) => {
         await pgAsync.query(`
             CREATE TABLE "user_roles" (
-                role_name character varying(30),
                 user_id serial NOT NULL,
+                role_name character varying(30),
                 PRIMARY KEY (user_id),
-                FOREIGN KEY (role_name) REFERENCES roles(role) ON UPDATE CASCADE ON DELETE CASCADE,
-                FOREIGN KEY (user_id) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE
+                FOREIGN KEY (user_id) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE,
+                FOREIGN KEY (role_name) REFERENCES roles(role) ON UPDATE CASCADE ON DELETE CASCADE
             );
         `)
 
         await pgAsync.query(`
-            INSERT INTO "user_roles" (role_name, user_id) VALUES ('admin', 1);
+            INSERT INTO "user_roles" (user_id, role_name) VALUES (1, 'admin');
         `)
     })
 }
