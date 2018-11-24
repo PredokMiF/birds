d098f47cd8f5b0eb45f383fe48b436a6211f832c
StepanPG <stepan-mandarin@yandex.ru>
2018-11-24 20:17:53 +0300

    fix: added user info to response

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,3 +1,10 @@
 module.exports = async function user(ctx) {
-    ctx.body = { authorized: ctx.isAuthenticated() }
+    ctx.body = { 
+        authorized: ctx.isAuthenticated(),
+        user: {
+            id: ctx.state.user.id,
+            login: ctx.state.user.login,
+            roles: ctx.state.user.roles
+        }
+    }
 }
