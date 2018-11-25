5fd160a2ede9c3eb5e03f21c9fa8c69a94959aa3
Predok_MiF <grigorchuk.konstantin@gmail.com>
2018-11-25 11:04:23 +0300

    1

--- server/src/dao/birdPost.js
+++ server/src/dao/birdPost.js
@@ -3,7 +3,7 @@ const pgAsync = require('../db')
 module.exports = async function birdPost(data, user_id, approved = false, approver_id = null) {
     return await pgAsync.query(`INSERT INTO "bird" (date_of_record, latitude, longitude, bird_type, metal_ring_id, bird_gender, bird_age, circumstances, approved, user_id, approver_id) 
 	    VALUES (
-	    CURRENT_TIMESTAMP,
+	    '${data.date_of_record}',
 	    '${data.latitude}',
 	    '${data.longitude}',
 	    '${data.bird_type}',
--- server/src/utils/getValidatedIdNumber.js
+++ server/src/utils/getValidatedIdNumber.js
@@ -1,13 +1,15 @@
 module.exports = function getValidatedIdNumber(id){
     let validatedId = '';
     if (
-        id[id.length - 1].match(/[a-z]/ig) && 
-        id.match(/[0-9]/g).length === id.length && 
+        id[id.length - 1].match(/[a-z]/ig) &&
+        id.match(/[0-9]/g).length === id.length &&
         id.length < 10) {
         validatedId = '.'.repeat(10 - id.length) + id;
     } else if (id.length < 10) {
         let matched = id.match(/^([0-9a-z]*[a-z])?(\d*)$/i);
-        if (!matched[2]) {
+        if (!matched) {
+            validatedId = '..........'
+        } else if (!matched[2]) {
             validatedId = (matched[2] || '') + '.'.repeat(10 - id.length) + (matched[1] || '');
         } else {
             validatedId = (matched[1] || '') + '.'.repeat(10 - id.length) + (matched[2] || '');
